cf8fe43081484fe331542d63b5472634ecbeab4b
milindchauhan <milindchauhan95@gmail.com>
2025-05-27 20:29:04 +0530

    fix typo in sql

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,9 +1,9 @@
 -- +goose Up
-CREATE TABLE users(
+CREATE TABLE users (
     id INTEGER UNIQUE,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name TEXT UNIQUE NOT NULL,
+    name TEXT UNIQUE NOT NULL
 );
 
 -- +goose Down
